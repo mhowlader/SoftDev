c8bdeae96f4fa7a4419255d55dc2ac95bbf54f4b
mhowlader <mhowlader@stuy.edu>
2018-10-04 21:59:39 -0400

    updated hw

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,43 +1,39 @@
-#Mohtasim Howlader, Aleksandra Koroza
-#SoftDev1 pd0
-#SQLITE3 BASICS
-#2018-10-04
+#Mohtasim Howlader, Aleksandra Koroza: Team Bitelguese
+#SoftDev1 pd8
+#K16 -- No Trouble
+#2018-10-05
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
 
 
-DB_FILE="discobandit.db"
+DB_FILE="discobandit.db" #delete before every subsequent run
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
 #==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
 
 with open('courses.csv', newline='') as coursefile:
      coursesDict = csv.DictReader(coursefile)
-     createCourses = "CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)"         #build SQL stmt, save as string
+     createCourses = "CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)"   #build SQL stmt, save as string
      c.execute(createCourses)   #run SQL statement
+
+     #each row is inserted into the courses table, code/mark/id being the column names.
      for row in coursesDict:
          c.execute(f'''INSERT INTO courses (code, mark, id) VALUES("{row['code']}","{ int(row['mark'])}","{int(row['id'])}")''')
 
 
 with open('peeps.csv', newline='') as peepsfile:
     peepsDict = csv.DictReader(peepsfile)
-    createPeeps = "CREATE TABLE peeps(name TEXT, age INTEGER, id INTEGER PRIMARY KEY)"         #build SQL stmt, save as string
+    createPeeps = "CREATE TABLE peeps(name TEXT, age INTEGER, id INTEGER PRIMARY KEY)"  #build SQL stmt, save as string
     c.execute(createPeeps)   #run SQL statement
+
+    #each row is inserted into the peeps table, name/age/id being the column names.
     for row in peepsDict:
-        c.execute(f'''INSERT INTO peeps (name, age, id) VALUES("{row['name']}","{ int(row['age'])}","{int(row['id'])}")''')
-        #c.execute("INSERT INTO peeps (name, age, id) VALUES ('hello', 5, 6)")
+        c.execute(f'''INSERT INTO peeps(name, age, id) VALUES("{row['name']}","{ int(row['age'])}","{int(row['id'])}")''')
 
-
-# createPeeps = "CREATE TABLE IF NOT EXISTS peeps(name TEXT, age INTEGER, id INTEGER)"         #build SQL stmt, save as string
-# c.execute(createPeeps)   #run SQL statement
-# for key, value in peepsDict.items():
-#     for obj in value:
-#         c.execute(f'INSERT INTO peeps VALUES({key}, {obj}')
 
 
 
