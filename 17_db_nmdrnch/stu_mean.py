516ebe65f1cf4e0ed3752067fdb9eee478c902dd
Mohtasim Howlader <mhowlader@stuy.edu>
2018-10-08 21:38:52 -0400

    updated hw

--- 17_db_nmdrnch/stu_mean.py
+++ 17_db_nmdrnch/stu_mean.py
@@ -1,5 +1,7 @@
-#Team Watch_Out_For_Snakes
-#Joan Chirinos, Mohtasim Howlader
+#Team Watch_Out_For_Snakes (Joan Chirinos, Mohtasim Howlader)
+#SoftDev1 pd8
+#K17 -- Average
+#2019-10-09
 
 
 import sqlite3   #enable control of an sqlite database
@@ -10,30 +12,31 @@ DB_FILE="discobandit.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
-def add_course_row(code, mark id):
+# adds a row to the courses table based on the given parameters
+def add_course_row(code, mark, id):
     c.execute('INSERT INTO courses VALUES({}, {}, {})'.format(course, mark, id))
 
 def go():
-    c.execute('CREATE TABLE peeps_avg(id INTEGER PRIMARY KEY, avg INTEGER)')
+    c.execute('CREATE TABLE peeps_avg(id INTEGER PRIMARY KEY, avg INTEGER)') #Create the peeps_avg table with two columns: ID as primary key, and avg for average
 
-    l = c.execute('SELECT peeps.id, mark FROM peeps, courses WHERE peeps.id = courses.id')
-    d = {}
+    l = c.execute('SELECT peeps.id, mark FROM peeps, courses WHERE peeps.id = courses.id') #l is set to an iterable cursor object with peep id that match courses id, and the corresponding mark for that course
+    d = {} #dictionary
 
-    for i in l:
-        if i[0] in d:
-            d[i[0]] += [i[1]]
+    for i in l: #
+        if i[0] in d: #checks if the id is already in the dictionary d
+            d[i[0]] += [i[1]] # if it is, add the mark to the values already there
         else:
-            d[i[0]] = [i[1]]
+            d[i[0]] = [i[1]] #if not, create a new key for the dictionary, and set its value to i[1] which is the mark
 
-    for i in d:
-        avg = sum(d[i]) / len(d[i])
-        c.execute('INSERT INTO peeps_avg VALUES({}, {})'.format(i, avg))
+    for i in d: #for each person
+        avg = sum(d[i]) / len(d[i]) #calculate average for each person by adding all of the marks and dividing by number of marks
+        c.execute('INSERT INTO peeps_avg VALUES({}, {})'.format(i, avg)) #populate table peeps_avg
 
     db.commit()
 
     x = c.execute('SELECT peeps.name, peeps.id, avg FROM peeps_avg, peeps WHERE peeps.id = peeps_avg.id')
-    for i in x:
-        print('name: {}\n\tid: {}\n\tavg: {}\n'.format(i[0], i[1], i[2]))
+    for i in x: #iterable table created in previous execute
+        print('name: {}\n\tid: {}\n\tavg: {}\n'.format(i[0], i[1], i[2])) #print student name, id, and average
 
     db.close()
 
