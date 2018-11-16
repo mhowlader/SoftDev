8fcf6d46c842d32c990cb2d23bac1f0684cc7ba9
mhowlader <mhowlader@stuy.edu>
2018-11-16 03:12:26 -0500

    updated 25

--- 25_rest/app.py
+++ 25_rest/app.py
@@ -14,9 +14,11 @@ def home():
     apiData = request.urlopen("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=1d18700111907e62e27adc5fa89fad1a")
     apiData = apiData.read()
     dict = json.loads(apiData)
+    icon = dict["weather"][0]["icon"]
+    iconUrl="http://openweathermap.org/img/w/" + icon + ".png"
 
     #return "Hello"
-    return render_template("home.html", expl = dict["weather"][0]["description"])
+    return render_template("home.html", img = iconUrl,  expl = dict["weather"][0]["description"])
 
 
 if __name__ == "__main__":
