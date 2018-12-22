3cc7c709213e8714fcc3b1bdab104235528eb844
mhowlader <mhowlader@stuy.edu>
2018-12-21 21:21:46 -0500

    update hw

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -2,6 +2,10 @@
 // SoftDev pd7
 // K30 -- sequential regression III: Season of the Witch
 // 2018-12-21
+
+var list = document.getElementById("thelist");
+
+
 var changeHeading = function(e) {
     var h = document.getElementById("h")
     //console.log(ele);
@@ -14,6 +18,7 @@ var removeItem = function(e) {
 
 var lis = document.getElementsByTagName("li");
 
+
 for(var i=0; i < lis.length; i++) {
     lis[i].addEventListener('mouseover', function(){ changeHeading(this.innerHTML)} );
     lis[i].addEventListener('mouseout', function(){ changeHeading("Hello World!")});
@@ -21,10 +26,12 @@ for(var i=0; i < lis.length; i++) {
 };
 
 var addItem = function(e) {
-    var list = document.getElementById("thelist");
     var item = document.createElement("li");
     item.innerHTML= "WORD";
     list.appendChild(item);
+    item.addEventListener('mouseover', function(){ changeHeading(this.innerHTML)} );
+    item.addEventListener('mouseout', function(){ changeHeading("Hello World!")});
+    item.addEventListener('click', function(){removeItem(this)});
  // ??? = "WORD";
  // ???
  // ...
@@ -35,6 +42,9 @@ var addItem = function(e) {
 var button = document.getElementById("b");
 button.addEventListener('click', addItem);
 
+//index of fibonacci
+var indFib = 1;
+
 var fib = function(n) {
     if(n < 2){
         return 1;
@@ -44,18 +54,43 @@ var fib = function(n) {
 };
 
 var addFib = function(e){
-    var list = document.getElementById("thelist");
     var item = document.createElement("li");
-    item.innerHTML= fib(5);
+    item.innerHTML= fib(indFib);
+    //console.log(fib(indFib));
     list.appendChild(item);
+    indFib++;
+
+    item.addEventListener('mouseover', function(){ changeHeading(this.innerHTML)} );
+    item.addEventListener('mouseout', function(){ changeHeading("Hello World!")});
+    item.addEventListener('click', function(){removeItem(this)});
 };
 
+// var fib2 = function(n) {
+//     var window = [0, 1, 1];
+//
+//     if(n < 3){
+//         return window[n];
+//     }else{
+//         var inc = 3;
+//         while(inc <= n){
+//             window[inc % 3] = window[(inc + 1) % 3] + window[(inc + 2) % 3];
+//             inc++;
+//         }
+//         return window[(inc - 1) % 3];
+//     }
+//
+// }
+//
 // var addFib2 = function(e){
-//     console.log(e);
-//  ???
-//  ...see QAF re: DYNAMIC PROGRAMMING...
-//  ???
+//     var item = document.createElement("li");
+//     item.innerHTML= fib2(indFib);
+//     list.appendChild(item);
+//     indFib++;
+//
+//     item.addEventListener('mouseover', function(){ changeHeading(this.innerHTML)} );
+//     item.addEventListener('mouseout', function(){ changeHeading("Hello World!")});
+//     item.addEventListener('click', function(){removeItem(this)});
 // };
 
-// var fb = document.getElementById("fb");
-// fb.addEventListener("click", addFib);
+var fb = document.getElementById("fb");
+fb.addEventListener("click", addFib);
